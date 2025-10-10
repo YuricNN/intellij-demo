e3c2b70be0e66294d63519376a65ddc669a4b2d4
halil <halil.yigit.0907@gmail.com>
2025-10-10 15:35:16 +0300

    update

---
+++ src/StaticLifetime.java
@@ -0,0 +1,25 @@
+public class StaticLifetime {
+
+    static int counter = 0;
+
+    public void incrementCounter() {
+
+        counter++;
+
+        System.out.println("Counter: " + counter);
+
+    }
+
+    public static void main(String[] args) {
+
+        StaticLifetime obj1 = new StaticLifetime();
+
+        StaticLifetime obj2 = new StaticLifetime();
+
+        obj1.incrementCounter();
+
+        obj2.incrementCounter();
+
+    }
+
+}
