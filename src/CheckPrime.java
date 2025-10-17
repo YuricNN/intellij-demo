65a9e161c6bfadfe4eae22275fce97b5d6e96c1f
halil <halil.yigit.0907@gmail.com>
2025-10-17 14:46:52 +0300

    new

--- src/CheckPrime.java
+++ src/CheckPrime.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class CheckPrime {
     // Method to check if a number is prime
     public static boolean isPrime(int n) {
-        // Handle case for numbers less than or equal to 1
+        
+         Handle case for numbers less than or equal to 1
         if (n <= 1) {
             return false;
         }
